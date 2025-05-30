362b68c981e0b39988ad3c6b3c9fdd467c4c6d27
tosuki <okaabe2006@gmail.com>
2025-05-29 21:23:39 -0300

    refactor: remove responsestatus annonation from errors

--- src/main/java/com/authservice/core/io/AuthError.java
+++ src/main/java/com/authservice/core/io/AuthError.java
@@ -1,21 +1,16 @@
 package com.authservice.core.io;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class AuthError extends CoreError {
     public AuthError(String level, String message) {
         super("Authentication", level, message);
     }
 
-    @ResponseStatus(code = HttpStatus.CONFLICT)
     public static class EmailConflict extends AuthError {
         public EmailConflict(String level, String email) {
             super(level, String.format("The email %s is occupied!", email));
         } 
     }
 
-    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
     public static class WrongCredentials extends AuthError {
         public WrongCredentials(String level) {
             super(level, "Wrong credentials");
